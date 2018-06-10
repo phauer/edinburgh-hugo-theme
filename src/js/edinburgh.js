8d099e6f7b0d20ff762c9cfebbe709824eb04341
Philipp Hauer <info@philipphauer.de>
2018-06-10 17:24:16 +0200

    define anchor-svg once and reuse it via svg's `<use>`.

--- src/js/edinburgh.js
+++ src/js/edinburgh.js
@@ -1,7 +1,9 @@
 (function() {
   function createAnchorHTML(heading) {
     return `
-      <a class="heading-anchor-link" href="${heading}"><svg class="anchor" height="22px" viewBox="0 0 24 24" width="22px" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"></path><path d="M3.9 12c0-1.71 1.39-3.1 3.1-3.1h4V7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h4v-1.9H7c-1.71 0-3.1-1.39-3.1-3.1zM8 13h8v-2H8v2zm9-6h-4v1.9h4c1.71 0 3.1 1.39 3.1 3.1s-1.39 3.1-3.1 3.1h-4V17h4c2.76 0 5-2.24 5-5s-2.24-5-5-5z"></path></svg></a>
+      <a class="heading-anchor-link" href="${heading}">
+        <svg width="18px" height="18px"><use href="#anchor"/></svg>
+      </a>
     `;
   }
 
